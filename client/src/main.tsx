c30b519b900633830d4d1a674a0c38918ca119de
SantiagoZarate <santiago2001zarate@gmail.com>
2024-09-05 11:03:33 -0300

    chore: move frontend to client folder

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { MainRouter } from './router/MainRouter.tsx';
+import { MainRouter } from './router/MainRouter';
 import './styles/index.css';
 
 createRoot(document.getElementById('root')!).render(
