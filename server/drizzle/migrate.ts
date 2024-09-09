1e725aacd10c65eda9b75c00f1f7b4f128ea9963
SantiagoZarate <santiago2001zarate@gmail.com>
2024-09-09 12:05:17 -0300

    chore: change eslint configs

--- server/drizzle/migrate.ts
+++ server/drizzle/migrate.ts
@@ -4,6 +4,6 @@ import { envs } from '../src/config/envs';
 
 if (envs.MIGRATE) {
   migrate(db, { migrationsFolder: './migrations' })
-    .then((response) => console.log('Migration succesfully done'))
+    .then((response) => console.log('Migration succesfully done', response))
     .catch((err) => console.log({ message: 'There was an error', err }));
 }
---
+++ server/eslint.config.js
@@ -0,0 +1,19 @@
+import js from '@eslint/js';
+import eslintPrettier from 'eslint-config-prettier';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  { ignores: ['dist'] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended, eslintPrettier],
+    files: ['**/*.ts'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    },
+  }
+);
